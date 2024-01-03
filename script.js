a6c6cd4bdb23f83cdcfa9ffe1dda88268638b975
izcheung <icheung24@my.bcit.ca>
2024-01-02 19:01:45 -0800

    Add modal pop up function

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
-function display_results(response){
-    $('#results').empty();
-    for (let i =0; i < response.results.length; i++){
-        // console.log(response)
-        
-        $('#results').append(
-            `
-            <div class="cards">
-                <h2>${response.results[i].title}</h2>
-                <img src="${response.results[i].image}">
-            </div>
-            `
-        )
-    }
-}
-
-
 function search_recipes(){
-    let query_input = $('#query_input').val()
+    let query_input_value = document.getElementById('query_input').value
+    console.log(query_input_value)
     try {
         $.ajax({
-            url: `https://api.spoonacular.com/recipes/complexSearch?apiKey=35119e298b824e7481b7febdf3fabcaa&query=${query_input}`,
+            url: `https://api.spoonacular.com/recipes/complexSearch?apiKey=35119e298b824e7481b7febdf3fabcaa&query=${query_input_value}`,
             type: 'GET',
             success: display_results
         });
@@ -27,3 +11,42 @@ function search_recipes(){
         alert("Unable to fetch Spoonacular API")
     }
 }
+
+function display_results(response){
+    $('#results').empty();
+    for (let i =0; i < response.results.length; i++){   
+        $('#results').append(
+            `
+            <div class="cards">
+                <h2>${response.results[i].title}</h2>
+                <img src="${response.results[i].image}">
+            </div>
+            `            
+        )  
+    }
+    $('#results').on("click", "div.cards", function(){
+        modal = document.getElementById('modal')
+        modal.showModal();
+    });
+}
+
+function close_modal(){
+    close_modal_button = document.getElementById('close_modal')
+    modal.close()
+}
+
+
+function setup(){
+
+    var query_input = document.getElementById('query_input')
+
+    query_input.addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+            document.getElementById("submit_button").click()
+            event.preventDefault();
+            console.log("enter key is pressed")
+        }   
+    });
+}
+
+$(document).ready(setup)
