c94f519baa5c97463a7e0ebb298cece02eddea84
izcheung <icheung24@my.bcit.ca>
2024-01-22 09:59:25 -0800

    Delete unused code

--- whatsinyourfridge/script.js
+++ whatsinyourfridge/script.js
@@ -5,9 +5,7 @@ function search_recipes(){
     console.log(query_input_value)
     try {
         $.ajax({
-            url: `https://api.spoonacular.com/recipes/findByIngredients?apiKey=35119e298b824e7481b7febdf3fabcaa&ingredients=${query_input_value}&ranking=1&number=3&ignorePantry=true`,
-            // url: `https://api.spoonacular.com/recipes/complexSearch?apiKey=35119e298b824e7481b7febdf3fabcaa&includeIngredients=${query_input_value}&number=15`,
-            // url: `https://api.spoonacular.com/recipes/random?apiKey=35119e298b824e7481b7febdf3fabcaa`,
+            url: `https://api.spoonacular.com/recipes/findByIngredients?apiKey=35119e298b824e7481b7febdf3fabcaa&ingredients=${query_input_value}&ranking=1&number=15&ignorePantry=true`,
             type: 'GET',
             success: [display_results, get_recipe_count],
         });
@@ -15,34 +13,6 @@ function search_recipes(){
         alert("Unable to fetch Spoonacular API")
     }
 }
-
-// function search_random_recipe(){
-//     let query_input_value = document.getElementById('query_input').value
-//     console.log(query_input_value)
-//     try {
-//         $.ajax({
-//             url: `https://api.spoonacular.com/recipes/random?apiKey=35119e298b824e7481b7febdf3fabcaa&number=3&tags=${query_input_value}`,
-//             type: 'GET',
-//             success: random_recipe_results,
-//         });
-//     } catch {
-//         alert("Unable to fetch Spoonacular API")
-//     }
-// }
-
-// function random_recipe_results(response){
-//     console.log(response)
-//     for (i = 0; i < response.recipes.length; i++){
-//         $('#results').append(
-//                     `
-//                     <div class="cards">
-//                         <h2>${response.recipes[i].title}</h2>
-//                         <img src="${response.recipes[i].image}">
-//                     </div>
-//                     `            
-//                 )  
-//         }
-//     }
 
 
 function display_results(response){
@@ -55,20 +25,7 @@ function display_results(response){
         </div>
         `
     )
-    // console.log(response)
-    // $('#results').empty();
-    
-    // for (let i = 0; i < response.results.length; i++){   
-    //     $('#results').append(
-    //         `
-    //         <div class="cards">
-    //             <h2>${response.results[i].title}</h2>
-    //             <img src="${response.results[i].image}">
-    //             <p>Ready in: ${response.results[i].title}</p>
-    //         </div>
-    //         `            
-    //     )  
-    // }
+
     console.log(response)
     $('#results').empty();
     
@@ -92,6 +49,7 @@ function display_results(response){
 
     });
 }
+
 
 function displayRecipeSummary(recipeId){
     try {
@@ -156,15 +114,12 @@ function openModalWithRecipeInfo(recipeId){
                     // Loop through each step and display information
                     for (let i = 0; i < steps.length; i++) {
                         const step = steps[i];
-                        const ingredientsList = step.ingredients.map(ingredient => ingredient.name).join(', ');
                       
 
                         $('#modal_body').append(
                             `<div class="step">
                                 <h3>Step ${step.number}</h3>
-                                <p>${step.step}</p>
-                                <p>Ingredients: ${ingredientsList}</p>
-                           
+                                <p>${step.step}</p>                           
                             </div>`
                         );
                     }
@@ -203,12 +158,8 @@ function setup(){
             document.getElementById("submit_button").click()
             event.preventDefault(); // prevents refresh
             // console.log("enter key is pressed")
-            
-
         }   
     });
-
- 
 }
 
 $(document).ready(setup)
